711d48db18cc2174d5b20889e784cd61e69f9840
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-07-17 22:59:33 +0000

    * just use URL.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/examples@505 2d713de4-4fbb-4d10-8406-bb89ef438322

--- full-text-search/test/unit/source_test.rb
+++ full-text-search/test/unit/source_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SourceTest < ActiveSupport::TestCase
   test "find by name" do
-    assert_equal(sources(:wikipedia_ja), Source.find_by_name("Wikipedia (ja)"))
+    assert_equal(sources(:wikipedia_ja).url,
+                 Source.find_by_name("Wikipedia (ja)").url)
   end
 end
