b7b6869e65ce96bd314272d862c279c25dad36ab
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-11-30 08:20:02 +0000

    * support search.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/examples@846 2d713de4-4fbb-4d10-8406-bb89ef438322

--- mail-archiver/searcher.rb
+++ mail-archiver/searcher.rb
@@ -23,4 +23,43 @@ class Searcher < Sinatra::Base
     @mail = Groonga::Record.new(@mails, Integer(id))
     haml :show
   end
+
+  get "/search/" do
+    @query = params[:query]
+    @mails = @mails.select do |record|
+      record["body"].match(@query)
+    end
+    haml :search
+  end
+
+  private
+  def highlight(body)
+    query = params[:query]
+    return html_escape(body) if query.nil?
+
+    expression_builder = Groonga::RecordExpressionBuilder.new(@mails, nil)
+    expression_builder.query = query
+    expression_builder.default_column = "body"
+    expression = expression_builder.build
+    _snippet = expression.snippet([["<span class=\"keyword\">", "</span>"]],
+                                  :width => body.size,
+                                  :html_escape => true,
+                                  :normalize => true)
+    segments = _snippet.execute(body)
+    if segments.empty?
+      html_escape(body)
+    else
+      segments.join
+    end
+  end
+
+  def snippet(record)
+    expression = record.table.expression
+    _snippet = expression.snippet([["<span class=\"keyword\">", "</span>"]],
+                                  :width => 100,
+                                  :html_escape => true,
+                                  :normalize => true)
+    separator = "\n<span class='separator'>...</span>\n"
+    _snippet.execute(record["body"]).join(separator)
+  end
 end
